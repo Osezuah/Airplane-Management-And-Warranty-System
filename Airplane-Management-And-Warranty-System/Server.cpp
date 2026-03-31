367bf83550cf2741da5d96d829a074e73c0a4985
Osezuah <tessosezuah@gmail.com>
2026-03-31 11:03:48 -0400

    SPRINT #1

--- Airplane-Management-And-Warranty-System/Server.cpp
+++ Airplane-Management-And-Warranty-System/Server.cpp
@@ -4,7 +4,7 @@
 
 
 int main() {
-	//SPRINT # 1 code
+	//SPRINT # 1 
 	//Airplane Management and Warranty System Server
 	// Connect to the PostgreSQL database v17.4
 	PGconn* conn = PQconnectdb(
--- Client/Client.cpp
+++ Client/Client.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main() {
-	//SPRINT #1 code
+	//SPRINT #1
 	//Airplane Management and Warranty System Client
 	// This client will read user input and send it to the server
 	string input;
