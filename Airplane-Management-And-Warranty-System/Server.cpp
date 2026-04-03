87ba87b1d68069e33d99a58244ff715de1baba7b
Lacenis <nicolasmasquieto@gmail.com>
2026-04-03 11:18:07 -0400

    Updated readme to explain port issues.

--- Airplane-Management-And-Warranty-System/Server.cpp
+++ Airplane-Management-And-Warranty-System/Server.cpp
@@ -20,42 +20,41 @@ int main() {
 	}
 	else {
 
-	//TCP Server
-	WSADATA wsData;
-	WORD ver = MAKEWORD(2, 2);
+		//TCP Server
+		WSADATA wsData;
+		WORD ver = MAKEWORD(2, 2);
 
-	int wsOk = WSAStartup(ver, &wsData);
-	if (wsOk != 0) {
-		std::cerr << "Can't initialize winsock!" << std::endl;
-		return 1;
+		int wsOk = WSAStartup(ver, &wsData);
+		if (wsOk != 0) {
+			std::cerr << "Can't initialize winsock!" << std::endl;
+			return 1;
+		}
+
+		SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+		sockaddr_in hint{};
+		hint.sin_family = AF_INET;
+		hint.sin_port = htons(54000);
+		hint.sin_addr.S_un.S_addr = INADDR_ANY;
+
+		bind(serverSocket, (sockaddr*)&hint, sizeof(hint));
+
+		//loop to keep the server constantly running
+		while (1) {
+			listen(serverSocket, SOMAXCONN);
+
+			SOCKET clientSocket = accept(serverSocket, nullptr, nullptr);
+
+			char buf[4096];
+			int bytesReceived = recv(clientSocket, buf, 4096, 0);
+
+			std::string response = "Processed: " + std::string(buf, bytesReceived);
+
+			send(clientSocket, response.c_str(), response.size(), 0);
+
+			cout << response;
+		}
+
+		return 0;
 	}
-
-	SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-
-	sockaddr_in hint{};
-	hint.sin_family = AF_INET;
-	hint.sin_port = htons(54000);
-	hint.sin_addr.S_un.S_addr = INADDR_ANY;
-
-	bind(serverSocket, (sockaddr*)&hint, sizeof(hint));
-
-	//loop to keep the server constantly running
-	while (1) {
-		listen(serverSocket, SOMAXCONN); 
-
-		SOCKET clientSocket = accept(serverSocket, nullptr, nullptr);
-
-		char buf[4096];
-		int bytesReceived = recv(clientSocket, buf, 4096, 0);
-
-		std::string response = "Processed: " + std::string(buf, bytesReceived);
-
-		send(clientSocket, response.c_str(), response.size(), 0);
-	// Start the server on port 8080
-	app.port(8080).multithreaded().run();
-
-		cout << response;
-	}	
-
-	return 0;
 }
--- README.md
+++ README.md
@@ -38,6 +38,19 @@ Run the following command on your Developer Command Prompt in Visual Studio or y
 docker-compose up -d
 ```
 
+**WARNING:** the database connects on localhost port 5432. If other processes are listening on the same port issues may arise.
+You can see if there are other processes listening on the same port use:
+```bash
+netstat -aon | findstr :5432
+```
+Look for the PID seen in collumn 5 of the output.
+You could use the following to see what process that PID corresponds to:
+```bash
+tasklist /fi "PID eq <insert PID here>"
+```
+You want to make sure that the "com.docker.backend.exe" process is listrning on port 5432, but no other process is.
+If another process is running on port 5432, stop it.
+
 ## **Step 4: Build**
 
 Build the Solution. This should take a long time as the required dependencies are downloaded through vcpkg.
