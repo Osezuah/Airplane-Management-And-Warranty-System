35965c826d1116f52001bbb260b3b0f08678fec1
Osezuah <tessosezuah@gmail.com>
2026-04-02 23:06:46 -0400

    connected the client and server

--- Airplane-Management-And-Warranty-System/Server.cpp
+++ Airplane-Management-And-Warranty-System/Server.cpp
@@ -1,33 +1,46 @@
-#include "asio.hpp"
-#include "crow.h"
-#include <libpq-fe.h>
+#pragma comment(lib, "Ws2_32.lib")
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <string>
+#include <iostream>
 
+using namespace std;
 
 int main() {
-	//SPRINT # 1 
-	//Airplane Management and Warranty System Server
-	// Connect to the PostgreSQL database v17.4
-	PGconn* conn = PQconnectdb(
-		"host=localhost port=5432 dbname=amws user=postgres password=Lkj876*bv"
-	);
-	if (PQstatus(conn) != CONNECTION_OK) {
-		fprintf(stderr, "Connection to database failed: %s", PQerrorMessage(conn));
-		PQfinish(conn);
+	//TCP Server
+	WSADATA wsData;
+	WORD ver = MAKEWORD(2, 2);
+
+	int wsOk = WSAStartup(ver, &wsData);
+	if (wsOk != 0) {
+		std::cerr << "Can't initialize winsock!" << std::endl;
 		return 1;
 	}
-	else {
-		printf("Connected to database successfully!\n");
-	}
-	// Initialize the Crow application
-	crow::SimpleApp app;
-	// Define a route for the root URL
-	CROW_ROUTE(app, "/")([]() {
-		return "Welcome to the Airplane Management and Warranty System!";
-		});
-	// Start the server on port 8080
-	app.port(8080).multithreaded().run();
 
-	//search http://localhost:8080 to see the welcome message
-    
+	SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+	sockaddr_in hint{};
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(54000);
+	hint.sin_addr.S_un.S_addr = INADDR_ANY;
+
+	bind(serverSocket, (sockaddr*)&hint, sizeof(hint));
+
+	//loop to keep the server constantly running
+	while (1) {
+		listen(serverSocket, SOMAXCONN); 
+
+		SOCKET clientSocket = accept(serverSocket, nullptr, nullptr);
+
+		char buf[4096];
+		int bytesReceived = recv(clientSocket, buf, 4096, 0);
+
+		std::string response = "Processed: " + std::string(buf, bytesReceived);
+
+		send(clientSocket, response.c_str(), response.size(), 0);
+
+		cout << response;
+	}	
+
 	return 0;
 }
