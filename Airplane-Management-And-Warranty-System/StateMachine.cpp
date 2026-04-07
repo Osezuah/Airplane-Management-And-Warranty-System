3493d7d674a393a09bb6f272a3935e976f7e2a9c
Lacenis <nicolasmasquieto@gmail.com>
2026-04-06 23:06:14 -0400

    Created Server State Machine

--- Airplane-Management-And-Warranty-System/StateMachine.cpp
+++ Airplane-Management-And-Warranty-System/StateMachine.cpp
@@ -1,1 +1,67 @@
 #include "StateMachine.h"
+
+ServerState StateMachine::GetState() const
+{
+    return currentState;
+}
+
+bool StateMachine::TransitionStateTo(ServerState newState)
+{
+    bool validTransition = false;
+
+    switch (currentState) {
+    case ServerState::IDLE:
+        if (newState == ServerState::WAITING_FOR_VERIFICATION) 
+            validTransition = true;
+        break;
+
+    case ServerState::WAITING_FOR_VERIFICATION:
+        if (newState == ServerState::CONNECTED)
+            validTransition = true;
+        break;
+
+    case ServerState::CONNECTED:
+        if (newState == ServerState::PROCESSING_REQUEST)
+            validTransition = true;
+        if (newState == ServerState::IDLE)
+            validTransition = true;
+        break;
+
+    case ServerState::PROCESSING_REQUEST:
+        if (newState == ServerState::TRANSFERRING_DATA)
+            validTransition = true;
+        if (newState == ServerState::CONNECTED)
+            validTransition = true;
+        break;
+
+    case ServerState::TRANSFERRING_DATA:
+        if (newState == ServerState::CONNECTED)
+            validTransition = true;
+        break;
+
+    case ServerState::ERROR:
+        if (newState == ServerState::IDLE)
+            validTransition = true;
+        break;
+    }
+
+    if (validTransition || newState == ServerState::ERROR) {
+        currentState = newState;
+        return true;
+    }
+
+    return false;
+}
+
+std::string StateMachine::StateToString()
+{
+    switch (currentState) {
+    case ServerState::IDLE: return "IDLE";
+    case ServerState::WAITING_FOR_VERIFICATION: return "WAITING_FOR_VERIFICATION";
+    case ServerState::CONNECTED: return "CONNECTED";
+    case ServerState::PROCESSING_REQUEST: return "PROCESSING_REQUEST";
+    case ServerState::TRANSFERRING_DATA: return "TRANSFERRING_DATA";
+    case ServerState::ERROR: return "ERROR";
+    default: return "UNKNOWN";
+    }
+}
--- Airplane-Management-And-Warranty-System/StateMachine.h
+++ Airplane-Management-And-Warranty-System/StateMachine.h
@@ -1,8 +1,25 @@
 #pragma once
 #include <string>
 
+enum class ServerState {
+	IDLE,
+	WAITING_FOR_VERIFICATION,
+	CONNECTED,
+	PROCESSING_REQUEST,
+	TRANSFERRING_DATA,
+	ERROR
+};
+
 class StateMachine
 {
+	ServerState currentState = ServerState::IDLE;
+public:
+	ServerState GetState() const;
 
+	// Returns true on successful transition,
+	// otherwise return false.
+	bool TransitionStateTo(ServerState newState);
+
+	std::string StateToString();
 };
 
