dfbdc8dc4189261695ebccd8b02790cb7551df59
Lacenis <nicolasmasquieto@gmail.com>
2026-04-17 05:14:54 -0400

    omitted client tests

--- Tests/ClientTests.cpp
+++ Tests/ClientTests.cpp
@@ -1,25 +1,25 @@
 #include "pch.h"
-#include "CppUnitTest.h"
-#include <winsock.h>
-
-using namespace Microsoft::VisualStudio::CppUnitTestFramework;
-
-namespace ClientTests
-{
-	TEST_CLASS(Client_Tests)
-	{
-	public:
-
-		TEST_METHOD(UT_CLT_01_ClientTerminateConnection)
-		{
-			//ARRANGE
-
-
-			//ACT
-
-
-			//ASSET
-
-		}
-	};
-}
+//#include "CppUnitTest.h"
+//#include <winsock.h>
+//
+//using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+//
+//namespace ClientTests
+//{
+//	TEST_CLASS(Client_Tests)
+//	{
+//	public:
+//
+//		TEST_METHOD(UT_CLT_01_ClientTerminateConnection)
+//		{
+//			//ARRANGE
+//
+//
+//			//ACT
+//
+//
+//			//ASSET
+//
+//		}
+//	};
+//}
